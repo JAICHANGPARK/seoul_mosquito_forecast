caf589eaa4e913d1699b6610484b852ec83de98b
Dreamwalker <aristojeff@gmail.com>
2020-08-01 15:56:47 +0900

    Update mosquito_forecast_app.dart

--- lib/ui/screen/mosquito_forecast_app.dart
+++ lib/ui/screen/mosquito_forecast_app.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 
 class MosquitoForecastApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      initialRoute: "/",
+      routes: {
+        "/" : (context) => HomePage()
+      },
+    );
   }
 }
