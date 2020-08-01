bdcec8783b67ea159a45e38b0ed14f88a57d70bc
Dreamwalker <aristojeff@gmail.com>
2020-08-01 15:55:54 +0900

    Create mosquito_forecast_app.dart

---
+++ lib/ui/screen/mosquito_forecast_app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+
+class MosquitoForecastApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
