7d4176275cd5088360d5b21c5ce1c1af77c6001f
Dreamwalker <aristojeff@gmail.com>
2020-08-01 16:05:00 +0900

    Update home_page.dart

--- lib/ui/screen/home_page.dart
+++ lib/ui/screen/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_seoul_mosquito_forcast/repository/mosquito_api.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -8,7 +9,13 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchMosquitoForecast();
+  }
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
